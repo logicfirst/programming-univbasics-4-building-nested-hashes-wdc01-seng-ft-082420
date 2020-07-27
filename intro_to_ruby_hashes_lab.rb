770b5cac0c2ebd93465c16da52cd934ec17e16c8
Scott H Lee <slee@logicfirstllc.com>
2020-07-27 14:36:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,16 @@ def monopoly_with_third_tier
   # complexity.
    monopoly = {:railroads => {
     :pieces => 4, 
-    :rent_in_dollars => {:one_piece_owned => "25"}, 
-    :names => {}
-    }
+    :rent_in_dollars => {:one_piece_owned => "25",
+                         :two_pieces_owned => "50",
+                         :three_pieces_owned => "100",
+                         :four_pieces_owned => "200"
+                        }, 
+    :names => {:reading_railroad => {},
+               :pennsylvania_railroad => {},
+               :b_and_o_railroad => {}
+              }
+   }
   }
 
 end
